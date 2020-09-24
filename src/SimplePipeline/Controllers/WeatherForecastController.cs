19131a46f71fa4b91c1ca58ccd15002d2b54d462
Zwelithini Mphanza <z.mphanza@gmail.com>
2020-09-24 19:19:25 +0200

    Initial commit

---
+++ src/SimplePipeline/Controllers/WeatherForecastController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SimplePipeline.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "The temperature is 34 degrees celcius";
+        }
+    }
+}
---
+++ test/SimplePipeline.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using SimplePipeline.Controllers;
+
+namespace SimplePipeline.Test
+{
+    public class UnitTest1
+    {
+        WeatherForecastController controller = new WeatherForecastController();
+        [Fact]
+        public void GetReturnsTemperatureString()
+        {
+            var temp = controller.Get();
+            Assert.Equal("The temperature is 34 degrees celcius", temp.Value);
+        }
+    }
+}
